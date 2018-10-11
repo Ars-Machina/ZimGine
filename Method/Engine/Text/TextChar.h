e72a16ae86bdcb8ab867763718f7a9180fe67d1f
brandon zimmerman <bzimmerman100@gmail.com>
2018-10-11 15:30:14 -0500

    commit 4 10/11/18

--- Method/Engine/Text/TextChar.h
+++ Method/Engine/Text/TextChar.h
@@ -5,6 +5,7 @@
 #include <map>
 
 void RenderText(Shader &shader, string text, GLfloat x, GLfloat y, GLfloat scale, vec3 color);
+void setupText();
 
 struct Character {
 	GLuint TextureID;
@@ -16,6 +17,14 @@ struct Character {
 map<GLchar, Character> Characters;
 
 void RenderText(Shader &shader, string text, GLfloat x, GLfloat y, GLfloat scale, vec3 color) {
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	string::const_iterator c;
 	for (c = text.begin(); c != text.end(); c++) {
 		Character ch = Characters[*c];
@@ -26,17 +35,34 @@ void RenderText(Shader &shader, string text, GLfloat x, GLfloat y, GLfloat scale
 		GLfloat w = ch.Size.x * scale;
 		GLfloat h = ch.Size.y * scale;
 
-		GLfloat vertices[6][4] = {
-			{xPos, yPos + h, 0.0, 0.0},
-			{xPos, yPos, 0.0,1.0},
-			{xPos + w, yPos, 1.0,1.0},
-
-			{xPos, yPos + h, 0.0,0.0},
-			{xPos + w, yPos, 1.0,1.0},
-			{xPos + w, yPos + h, 1.0,0.0}
-		};
+		
 	}
 }
 
+void setupText() {
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	for (GLubyte c = 0; c < 128; c++) {
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+			cout << "ERROR::FREETYPE: Failed to load Glyph" << endl;
+			continue;
+		}
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, face->glyph->bitmap.width, face->glyph->bitmap.rows, 0, GL_RED, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		Character character = {
+			texture, ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Characters.insert(pair<GLchar, Character > (c, character));
+	}
+}
 
 #endif
